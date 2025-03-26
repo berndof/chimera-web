c55382f3cd4453f00156a9204124fef920dc2cce
berndof <berndofb@gmail.com>
2025-03-26 20:26:45 -0300

    fixed optional role detail

--- backend/app/core/__init__.py
+++ backend/app/core/__init__.py
@@ -7,6 +7,6 @@ from .user import router as user_router
 
 
 def register(app: FastAPI):
-    app.include_router(user_router)
     app.include_router(auth_router)
+    app.include_router(user_router)
     app.include_router(role_router)
--- backend/app/core/role/schemas.py
+++ backend/app/core/role/schemas.py
@@ -11,13 +11,11 @@ from .models import Role
 class RoleBase(BaseSchema[Role]):
     name: Annotated[
         str, StringConstraints(min_length=3, max_length=30, pattern=r"^[a-z_]+$")
-    ] = Field(..., example="administrator")
-    detail: (
-        Annotated[
-            str, StringConstraints(min_length=3, max_length=30, pattern=r"^[A-Za-z_]+$")
-        ]
-        | None
-    ) = Field(..., example="string")
+    ] = Field(..., example="default_users")
+    detail: Annotated[
+        str | None,
+        StringConstraints(min_length=3, max_length=30, pattern=r"^[A-Za-z_]+$"),
+    ] = Field(None, example="notes...")
 
 
 class RoleFilter(BaseFilter[RoleBase]):
